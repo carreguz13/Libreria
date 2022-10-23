556f047078610432500376c66da078ceb0fd7b93
carlos <carreguz13@gmail.com>
2022-10-23 17:54:26 -0600

    Llamar data del objeto de libros para reflejarla en libreriaPage

--- src/componentes/LibreriaPage/LibrosLista.js
+++ src/componentes/LibreriaPage/LibrosLista.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import "./LibrosLista.css"
+import Libros from 'C:/Users/carlos/Desktop/Proyecto-Libreria/libreria/src/componentes/Libros'
+/*import {Button, Table} from "react-bootstrap"*/
+/*import "bootstrap/dist/css/bootstrap.min.css"*/
+
 
 function LibrosLista() {
   return (
-    <div>libreria</div>
+    <Fragment>
+         <div className='lol' >
+          <div className='lol2'>
+          {
+          Libros && Libros.length > 0
+          ?
+          Libros.map((item) =>{
+            return(
+              <div>
+              <div>
+               <h3>Autor:{item.Autor} </h3>
+              </div>
+               <div>
+               <h3>Titulo:{item.Titulo} </h3>
+             </div>
+              <div>
+              <h3>Edicion:{item.Edicion} </h3>
+              </div>
+              </div>
+            )
+          })
+          :
+          "No data available"
+        }
+        </div>
+        </div>
+    </Fragment>
   )
 }
 
--- src/componentes/Libros.js
+++ src/componentes/Libros.js
@@ -2,9 +2,9 @@ const Libros =  [
 /*creamos un array que contiene tres objetos (informacion del libro)*/ 
 {
     id:"1",
-    Autor :"John Green",
-    Título:"Paper Towns",
-    Edición :"primera edición",
+    Autor:"John Green",
+    Titulo:"Paper Towns",
+    Edicion:"primera edición",
     Fecha_de_publicación:"07/24/2015",
     Tipo_de_contenido:"Juvenil",
     Restricciones:"no hay restricciones",
@@ -14,8 +14,8 @@ const Libros =  [
 {
     id:"2",
     Autor :"will suarez",
-    Título:"Just in the sea",
-    Edición :"Tercera edición",
+    Titulo:"Just in the sea",
+    Edicion :"Tercera edición",
     Fecha_de_publicación:"11/08/1984",
     Tipo_de_contenido:"Científico",
     Restricciones:"no hay restricciones",
@@ -25,8 +25,8 @@ const Libros =  [
 {
     id:"3",
     Autor :"Britney smith",
-    Título:"Around Italy",
-    Edición :"Primera edicion",
+    Titulo:"Around Italy",
+    Edicion :"Primera edicion",
     Fecha_de_publicación:"02/02/2014",
     Tipo_de_contenido:"De viaje",
     Restricciones:"no hay restricciones",
