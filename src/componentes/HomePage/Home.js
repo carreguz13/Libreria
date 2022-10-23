83fa5bf3796092cb274e06a08c30fec0accc272c
carlos <carreguz13@gmail.com>
2022-10-23 16:18:06 -0600

    pagina home funcionalidad terminada

--- src/componentes/HomePage/Home.js
+++ src/componentes/HomePage/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./Home.css"
+/*importamos link para poder usar las routas de App.js y usarla con los botones*/ 
+import {Link} from "react-router-dom"
+
+
 
 function Home() {
   return (
-    <div>home</div>
+    <div className='main-container-home'>
+      <h1>Imagen Here</h1>
+{/*Botones Home*/} 
+
+        <Link to={"/registro"}>
+        <button>Registra un libro</button>
+        </Link>
+
+        <Link to={"/libreria"}>
+        <button>Libreria</button>
+        </Link>
+        
+    </div>
   )
 }
 
