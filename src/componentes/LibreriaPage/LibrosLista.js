fe264324fd0273748f873f3c65088c545417dfce
carlos <carreguz13@gmail.com>
2022-10-23 18:52:14 -0600

    avances en libreria

--- src/componentes/LibreriaPage/LibrosLista.js
+++ src/componentes/LibreriaPage/LibrosLista.js
@@ -3,34 +3,52 @@ import "./LibrosLista.css"
 import Libros from 'C:/Users/carlos/Desktop/Proyecto-Libreria/libreria/src/componentes/Libros'
 /*import {Button, Table} from "react-bootstrap"*/
 /*import "bootstrap/dist/css/bootstrap.min.css"*/
+import {Link} from "react-router-dom"
 
 
 function LibrosLista() {
   return (
     <Fragment>
          <div className='lol' >
-          <div className='lol2'>
+          <Link to={"/"}>
+          <button>back home</button>
+          </Link>
+<div className='tabla-contenedor'>
+          <div className='informacion'>
           {
           Libros && Libros.length > 0
           ?
           Libros.map((item) =>{
             return(
+              <div className='informacion'>
+              <div className='primera'>
               <div>
-              <div>
-               <h3>Autor:{item.Autor} </h3>
+              {item.Autor}
               </div>
                <div>
-               <h3>Titulo:{item.Titulo} </h3>
+               {item.Titulo}
              </div>
               <div>
-              <h3>Edicion:{item.Edicion} </h3>
+              {item.Edicion}
+              </div><br></br>
+              </div>
+              <div className='botones'>
+              <h2>loq ue sea </h2>
               </div>
               </div>
+              
             )
           })
           :
           "No data available"
+          
         }
+       
+       
+
+
+        </div>
+ 
         </div>
         </div>
     </Fragment>
